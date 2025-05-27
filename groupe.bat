168fbf7920524faced7f2008000992ece8a1afcd
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-05-27 09:48:28 +0200

    Update groupe.bat
    groupe.bat now invokes groupe.ps1

--- groupe.bat
+++ groupe.bat
@@ -1,11 +1,9 @@
 @echo OFF
-:: demande le groupe si aucun groupe nest fourni
-if "%1"=="" (
-set /p filtre="groupe="
-) else (
-:: recupere le premier argument
-set filtre=%1
-)
-:: invoque une commande powershell qui liste nom prenom login, trie selon le login et affiche sous forme de tableau
-powershell -command "Get-ADGroupMember -Identity '%filtre%' | Select-Object Name, SamAccountName | Sort-Object SamAccountName | Format-Table -AutoSize"
-pause
+:: if "%1"=="" (
+:: set /p filtre="groupe="
+:: ) else (
+:: set filtre=%1
+:: )
+powershell.exe -ExecutionPolicy Bypass -File "groupe.ps1" %*
+:: powershell -command "Get-ADGroupMember -Identity '%filtre%' | Select-Object Name, SamAccountName | Sort-Object SamAccountName | Format-Table -AutoSize"
+:: pause
